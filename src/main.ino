a5215431c0313140e514dd7322399306c07ac44a
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-12-23 22:55:37 +0200

    Init commit

---
+++ src/main.ino
@@ -0,0 +1,26 @@
+const int ledPins[] = {2, 3, 4, 5, 6};
+const int ledSize = sizeof(ledPins);
+
+void allOnOff() {
+  const int SLEEPTIME = 1000;
+
+  for (int i = 0; i < ledSize; i++) {
+    digitalWrite(ledPins[i], HIGH);
+  }
+  delay(SLEEPTIME);
+
+  for (int i = 0; i < ledSize; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+  delay(SLEEPTIME);
+}
+
+void setup() {
+  for (int i = 0; i < ledSize; i++) {
+   pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  allOnOff();
+}
